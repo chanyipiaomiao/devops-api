54f165370f61904593ad6d19adfe7bcba4bb45cb
helei <chanyipiaomiao@163.com>
2018-02-28 09:22:14 +0800

    更新.goreleaser.yml build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# 程序名称
-APP_NAME=devops-api
-
-# 程序的版本
-APP_VERSION=0.1.0
-
 # 编译时的Go版本
 GO_VERSION=`go version | awk '{print $3}'`
 
-# Git 提交时的ID
-COMMIT_HASH=`git rev-parse HEAD 2>/dev/null`
-
-# 编译日期时间
-BUILD_DATE=`date "+%F %H:%M:%S"`
-
-goreleaser --rm-dist
+GO_VERSION goreleaser --rm-dist
